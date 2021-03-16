cfd5c380fa62f427b1509bde540a2cc12fd3f35d
Vikum-Kule <56560837+Vikum-Kule@users.noreply.github.com>
2021-03-16 21:40:54 +0530

    user name updated

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -297,13 +297,13 @@ class Users extends Controller{
     }
 
     public function createUserSession_pc($user){
-        $_SESSION['username'] = $user->username;
+        $_SESSION['username'] = $user->userName;
 	    $_SESSION['email'] = $user->email;
         header('location:' . URLROOT . '/pc_view_drug/show_orders');
     }
 
     public function createUserSession_man($user){
-        $_SESSION['username'] = $user->username;
+        $_SESSION['username'] = $user->userName;
 	    $_SESSION['email'] = $user->email;
         header('location:' . URLROOT . '/Man_Adddrug/showdrugs');
     }
