4e6f68564d2de39d7169d59ae3a9cc9fef251712
deeghayu <deefernando6@gmail.com>
2021-03-23 21:17:03 +0530

    change form layout

--- app/views/Man_Add_Supplier_Order.php
+++ app/views/Man_Add_Supplier_Order.php
@@ -39,7 +39,7 @@
 
   <form action="<?php echo URLROOT; ?>/Man_supplier_order/addorder" method="post">
    
-  <div class="container">
+ <!-- <div class="container">
      <h1>Add Supplier Order</h1>
       <hr>
   
@@ -58,8 +58,58 @@
       <button type="submit" class="registerbtn">ADD</button>
         
     </div>
+    -->
     
-    
+    <div class="row">
+            <div class="col-8 col-m-12 col-sm-12" id="inputTable">
+                <div class="card">
+                    <div class="card-content">
+                    <form ‍>
+                    
+                    <input type="hidden" name="rowNum" id="rowNum">
+                        <table id="openTab">
+                            <tbody>
+                                <tr>
+                                <form method="POST">
+                                    <th>Order ID</th>
+                                    <th><input type="text" placeholder="Enter Order ID" name="orderid" id="orderid" required> </th>
+                                </form>
+                                </tr>
+                                <tr>
+								<form >
+                                    <th>Supplier Id</th>
+                                    <th><input type="text" placeholder="Enter SupplierId" name="supplierId" id="supplierId" required> </th>
+                                </form>
+                                </tr>
+                                <tr>
+                                    <th>Invoice No</th>
+                                    <th>
+                                    <input type="number" placeholder="Enter invoice No" name="invoiceNo" id="invoiceNo" required>
+                                    </th>
+                                    </tr>
+                                <tr>
+                                    <th>Price</th>
+                                    <th>
+                                    <input type="price" placeholder="Enter price" name="price" id="price" required>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>
+                                    <input type="Date" placeholder="Date" name="date" id="date" required>
+                                    </th>
+                                </tr>
+                            
+                                </tbody>
+                        </table>
+        </form>
+         </div>
+         <div style="text-align: center;height:50px;">
+                    <button type="submit" name="submit" class="registerbtn">ADD</button>
+                    </div>
+                </div>
+            </div>
+
   </form>
   
   </div>
