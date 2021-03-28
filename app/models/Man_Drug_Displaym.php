cbcd3d5395e41ded2e119c646474f698a2fdf612
deeghayu <deefernando6@gmail.com>
2021-03-28 17:39:18 +0530

    update inquiries display

--- app/models/Man_Drug_Displaym.php
+++ app/models/Man_Drug_Displaym.php
@@ -18,7 +18,24 @@
             $count = $this->db->single();
             return $count;
         }
-
+        public function count_unread(){
+            $this->db->query("SELECT COUNT(inquiryId) as count FROM inquiry WHERE status=="unread";");
+           
+            $count = $this->db->single();
+            return $count;
+        }
+        public function count_processing(){
+            $this->db->query("SELECT COUNT(inquiryId) as count FROM inquiry WHERE status=="processing";");
+           
+            $count = $this->db->single();
+            return $count;
+        }
+        public function count_completed(){
+            $this->db->query("SELECT COUNT(inquiryId) as count FROM medinquiryicine WHERE status=="completed";");
+           
+            $count = $this->db->single();
+            return $count;
+        }
 
 
         
